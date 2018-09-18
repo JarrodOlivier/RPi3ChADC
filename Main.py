c1399b41ba4b4f3ce93df17235872ba3f18ed291
Jarrod Olivier <42741227+JarrodOlivier@users.noreply.github.com>
2018-09-18 18:37:38 +0200

    Main.py
    Added interrupts to GPIO pins that corresponds to the hardware buttons

--- Main.py
+++ Main.py
@@ -9,17 +9,16 @@ Time = [0,0,0,0,0]
 Timer = [0,0,0,0,0]
 ch0 = [0,0,0,0,0]
 ch1 = [0,0,0,0,0]
-ch2 = [0,0,0,0,0]
+ch2 = [0,0,0,0,0]                             
 
-count = 0											        #Determines T value determined by frequency button
+count = 0				              #Determines T value determined by frequency button
 T = 0.5                               #This value is frequency
-i = 0													        #Linked to frequency
+i = 0						          #Linked to frequency
 
 index = 0
 
 stopState = 0
-wall = time.time()
-    
+wall = time.time()  
 
 # Software SPI configuration:
 CLK  = 18
@@ -40,7 +39,7 @@ GPIO.setup(frequency,GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(stop,GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(display,GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
-def timerFormat(t):
+def timerFormat(t):                             #Displays time in MM:SS:mm
     t = t*10
     milli = t % 10
     t //= 10
@@ -48,3 +47,33 @@ def timerFormat(t):
     t //= 60
     mins = t
     return '%02d:%02d.%d0' % (mins, sec, milli)
+
+########## Callback Fns #############################################
+
+def stopOP(channel):
+    global stopState
+    if(stopState == 1):
+        stopState = 0
+    else:
+        stopState = 1
+        stopFn()
+    time.sleep(0.5)
+    return stopState
+	
+def frequencyOP(channel):
+    Tvalue()
+def resetOP(channel):
+    resetFn()
+def displayOP(channel):
+    
+    global index, T
+    index = index + 1
+    print("Button pressed: ", index, "Frequency: ", T)
+    displayFn()
+######################################################################
+
+#Interrupts
+GPIO.add_event_detect(reset, GPIO.FALLING, callback = resetOP, bouncetime = 500)
+GPIO.add_event_detect(frequency, GPIO.FALLING, callback = frequencyOP, bouncetime = 500)
+GPIO.add_event_detect(stop, GPIO.FALLING, callback = stopOP, bouncetime = 200)
+GPIO.add_event_detect(display, GPIO.FALLING, callback = displayOP, bouncetime = 4000)       
